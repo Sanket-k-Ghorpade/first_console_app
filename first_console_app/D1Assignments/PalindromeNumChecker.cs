92cff77cba6433d4d10304ccf755a52e2c44d2ea
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-06-16 07:18:51 -0700

    Add files via upload

---
+++ first_console_app/D1Assignments/PalindromeNumChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.D1Assignments
+{
+    public class PalindromeNumChecker
+    {
+        public static void Run()
+        {
+            Console.Write("Enter your number to check if its palindrome or not: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int copyNum = num;
+            int reverse = 0;
+            while (num > 0)
+            {
+                int lastDigit = num % 10;
+                reverse = lastDigit + reverse* 10;
+                num /= 10;
+            }
+            if (copyNum == reverse) Console.WriteLine("Palindrome");
+            else Console.WriteLine("Not Palindrome");
+        }
+    }
+}
