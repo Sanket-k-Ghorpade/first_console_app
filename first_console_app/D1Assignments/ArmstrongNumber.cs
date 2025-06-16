b137f977cc6c71e3ea28c3fdff4e45824762308f
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-06-16 07:44:56 -0700

    Add files via upload

---
+++ first_console_app/D1Assignments/ArmstrongNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.D1Assignments
+{
+    public class ArmstrongNumber
+    {
+        public static void Run()
+        {
+            Console.Write("Enter your number to check if its Armstrong or not: ");
+            string numStr = Console.ReadLine();
+            int length = numStr.Length;
+
+            int num = Convert.ToInt32(numStr);
+            int copyNum = num;
+            int armStrong = 0;
+
+            while (num > 0) {
+                int lastDigit = num % 10;
+                armStrong += (int)Math.Pow(lastDigit, length);
+                num /= 10;
+            }
+            if (armStrong == copyNum) Console.WriteLine($"{copyNum} is an Armstrong number.");
+            else Console.WriteLine($"{copyNum} is not an Armstrong number.");
+        }
+    }
+}
---
+++ first_console_app/D1Assignments/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.D1Assignments
+{
+    public class ReverseNumber
+    {
+        public static void Run()
+        {
+            Console.Write("Enter a number to be reversed: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int reversed = 0;
+            while (num > 0)
+            {
+                int lastDigit = num % 10;
+                reversed = lastDigit + reversed * 10;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse is "+reversed);
+        }
+    }
+}
