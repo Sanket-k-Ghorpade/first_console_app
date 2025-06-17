af829f760878f679598d0ee28932562bf5e7a644
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-06-17 06:36:22 -0700

    Update Program.cs

--- first_console_app/Program.cs
+++ first_console_app/Program.cs
@@ -6,48 +6,6 @@ namespace first_console_app
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello, World!");
-
-            // initialization and declaration 
-            //int a = 10,b= 20;
-            //int sum = a + b;
-            //Console.WriteLine("The sum before user input is "+sum);
-
-            //// User input
-            //Console.WriteLine("Enter first number :");
-            //a = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter second number :");
-            //b = Convert.ToInt32(Console.ReadLine());
-            //sum = a + b;
-            //Console.WriteLine("The sum after accepting user input is " + sum);
-
-            /*
-            Console.WriteLine("Enter a charecter input: ");
-            char char_a = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Your char input is " + "'" + char_a + "'");
-            */
-
-            //odd or even
-            //Console.WriteLine("Enter a number");
-            //int num = Convert.ToInt32(Console.ReadLine());
-
-            ////if(num % 2 == 0) { Console.WriteLine("Its an Even number"); }
-            ////else { Console.WriteLine("Its an odd number"); }
-
-            ////string op = (num % 2 == 0) ? "Even" : "Odd";
-            ////Console.WriteLine(op);
-
-            //if (num % 5 == 0)
-            //{
-            //    Console.WriteLine("Divisible by 5");
-
-            //}
-            //else if (num % 2 == 0) {
-            //    Console.WriteLine("Divisible by 2");
-            //}
-            //else {
-            //    Console.WriteLine("Not divisible by 2 and 5");
-            //}
 
             //factorial.Run();
             //CountWords.Run();
@@ -58,6 +16,7 @@ namespace first_console_app
             //ReverseNumber.Run();
             //ArmstrongNumber.Run();
             //FibonacciSeries.Run();
+            //CountSpaces.Run();
 
         }
     }
