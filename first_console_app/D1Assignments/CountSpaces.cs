a46e1ff256270b83f31245bd4bb0bad92dd77ba9
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-06-17 06:38:53 -0700

    Add files via upload

---
+++ first_console_app/D1Assignments/CountSpaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.D1Assignments
+{
+    public class CountSpaces
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Enter the sentence: ");
+            string str = Console.ReadLine();
+            int spaceCount = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ') { spaceCount++; }
+            }
+            Console.WriteLine($"Total Space: {spaceCount}");
+        }
+    }
+}
