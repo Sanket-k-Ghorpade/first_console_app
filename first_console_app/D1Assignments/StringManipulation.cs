efcb4232920eb43d726adba51fdc8ac2dc37729d
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-06-18 06:28:10 -0700

    Add files via upload

---
+++ first_console_app/D1Assignments/StringManipulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.D1Assignments
+{
+    public class StringManipulation
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Enter the string: ");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split(',');
+            Console.WriteLine($"Developer name is: {strArray[0]}");
+            Console.WriteLine($"Company name is: {strArray[1]}");
+            Console.WriteLine($"Location is: {strArray[2]}");
+
+        }
+    }
+}
